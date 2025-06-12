2449499c09bb1e37c308bb74c5ac424313c90915
yingzi <GTyingzi@gmail.com>
2025-06-12 12:37:56 +0800

    add observabilty

---
+++ observability/src/main/java/com/spring/ai/tutorial/observability/ObservabilityApplication.java
@@ -0,0 +1,17 @@
+package com.spring.ai.tutorial.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yingzi
+ * @since 2025/6/12
+ */
+@SpringBootApplication
+public class ObservabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabilityApplication.class, args);
+    }
+
+}
