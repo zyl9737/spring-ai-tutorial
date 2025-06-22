ada1716d5d28a83e9fc4107d9903908f256719f3
yingzi <GTyingzi@gmail.com>
2025-06-22 15:58:53 +0800

    fix

--- rag/rag-evaluation/src/main/java/com/spring/ai/tutorial/rag/evaluation/RagEvaluationApplication.java
+++ rag/rag-evaluation/src/main/java/com/spring/ai/tutorial/rag/evaluation/RagEvaluationApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2025/6/11
  */
 @SpringBootApplication
-public class GraEvaluationApplication {
+public class RagEvaluationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraEvaluationApplication.class, args);
+        SpringApplication.run(RagEvaluationApplication.class, args);
     }
 }
